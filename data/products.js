3ed42ab9af608ef1e79c34b25de86de40635f312
omarkhair70-droid <omar.khair70@gmail.com>
2026-04-26 08:33:49 +0300

    Upgrade ordering flow with premium catalog and checkout

---
+++ data/products.js
@@ -0,0 +1,74 @@
+const products = [
+  {
+    id: 'brazilian-plain',
+    name: 'برازيلي سادة',
+    category: 'قهوة برازيلي',
+    type: 'سادة',
+    weight: '250 جم',
+    price: 60,
+    image: 'https://images.unsplash.com/photo-1497935586047-9397fe4794c0?auto=format&fit=crop&w=800&q=80',
+    description: 'تحميص متوسط بنكهة متوازنة وقوام ناعم مناسب للقهوة اليومية.'
+  },
+  {
+    id: 'brazilian-blend',
+    name: 'برازيلي محوج',
+    category: 'قهوة برازيلي',
+    type: 'محوج',
+    weight: '250 جم',
+    price: 60,
+    image: 'https://images.unsplash.com/photo-1511920170033-f8396924c348?auto=format&fit=crop&w=800&q=80',
+    description: 'خلطة برازيلي محوجة بعطر غني ولمسة بهارات دافئة.'
+  },
+  {
+    id: 'turkish-plain',
+    name: 'تركي سادة',
+    category: 'قهوة تركي',
+    type: 'سادة',
+    weight: '250 جم',
+    price: 50,
+    image: 'https://images.unsplash.com/photo-1509042239860-f550ce710b93?auto=format&fit=crop&w=800&q=80',
+    description: 'قهوة تركي سادة بطحن ناعم وقوام مركز لعشاق المذاق الصافي.'
+  },
+  {
+    id: 'turkish-blend',
+    name: 'تركي محوج',
+    category: 'قهوة تركي',
+    type: 'محوج',
+    weight: '250 جم',
+    price: 50,
+    image: 'https://images.unsplash.com/photo-1495474472287-4d71bcdd2085?auto=format&fit=crop&w=800&q=80',
+    description: 'قهوة تركي محوجة بطابع شرقي فاخر ورائحة مميزة.'
+  },
+  {
+    id: 'yemeni-plain',
+    name: 'يمني سادة',
+    category: 'قهوة يمني',
+    type: 'سادة',
+    weight: '250 جم',
+    price: 80,
+    image: 'https://images.unsplash.com/photo-1447933601403-0c6688de566e?auto=format&fit=crop&w=800&q=80',
+    description: 'بن يمني فاخر بنغمات عميقة وحموضة خفيفة متوازنة.'
+  },
+  {
+    id: 'yemeni-blend',
+    name: 'يمني محوج',
+    category: 'قهوة يمني',
+    type: 'محوج',
+    weight: '250 جم',
+    price: 85,
+    image: 'https://images.unsplash.com/photo-1442512595331-e89e73853f31?auto=format&fit=crop&w=800&q=80',
+    description: 'خلطة يمني محوجة بنكهة قوية وعمق واضح في كل فنجان.'
+  },
+  {
+    id: 'espresso',
+    name: 'إسبريسو',
+    category: 'قهوة إسبريسو',
+    type: 'محمص',
+    weight: '1000 جم',
+    price: 300,
+    image: 'https://images.unsplash.com/photo-1461023058943-07fcbe16d735?auto=format&fit=crop&w=800&q=80',
+    description: 'حبوب إسبريسو احترافية بكريمة ثابتة ونكهة شوكولاتة مكثفة.'
+  }
+];
+
+export default products;
